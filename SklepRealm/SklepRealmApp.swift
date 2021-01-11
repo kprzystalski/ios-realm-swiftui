a12b2c8fda5afe9f99264edb67ea86ac70faf632
Karol Przystalski <karol@codete.com>
2021-01-11 16:17:17 +0100

    Initial Commit

---
+++ SklepRealm/SklepRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SklepRealmApp.swift
+//  SklepRealm
+//
+//  Created by kprzystalski on 11/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SklepRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
